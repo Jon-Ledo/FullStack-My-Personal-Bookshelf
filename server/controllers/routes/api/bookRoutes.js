a0e101b441c0dcee2774a216cf5cbe0ece4de995
JonLedo <jonledo.code@gmail.com>
2022-12-03 18:02:05 -0500

    implemented basic handle bars setup. there is a bookshelf route in the server file (needs to be moved afterwards) and the data is bring pulled from the DB. added new folders, moved files as well.

---
+++ server/controllers/routes/api/bookRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+const sequelize = require('../../../config/connection')
+const { User, Book } = require('../../../models')
+
+module.exports = router
--- server/routes/api/bookRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const sequelize = require('../../config/connection')
-const { User, Book } = require('../../models')
-
-// routes here
-
-module.exports = router
