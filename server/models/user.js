6194a0067d17a200530441f6c2a150768bc46209
Joseph Mogavero <joseph.mogavero@hotmail.com>
2022-12-06 03:57:20 -0500

    added Bcrypt & Express-Session dependencies > auth

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection')
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -36,6 +41,13 @@ User.init(
     },
   },
   {
+   hooks: {
+    beforeCreate: async (newUserData) => {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
+    },
+  },
+
     sequelize,
     freezeTableName: true,
     underscored: true,
