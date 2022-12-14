b58812afa3cea9f5c21f179ad03af37145050d65
JonLedo <jonledo.code@gmail.com>
2022-12-14 15:10:31 -0500

    added a log out button to go with logout function

--- controllers/routes/api/userRoutes.js
+++ controllers/routes/api/userRoutes.js
@@ -135,7 +135,7 @@ router.post('/login', async (req, res) => {
 
 // logout
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end()
     })
--- public/js/signup.js
+++ public/js/signup.js
@@ -68,4 +68,4 @@ const logout = async () => {
   }
 }
 
-// document.querySelector('#logout').addEventListener('click', logout)
+document.querySelector('#logout').addEventListener('click', logout)
